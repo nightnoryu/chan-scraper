129aaed68201fcb8ca1c7652a4c83ba3f51b9c60
m3tro1d <qazwf734@gmail.com>
2020-06-04 20:26:41 +0300

    Add initial arguments

--- thread-parser.py
+++ thread-parser.py
@@ -5,5 +5,18 @@ import requests
 
 
 # Parse arguments
+parser = argparse.ArgumentParser(
+	description='''Downloads all files, images or videos from the thread on 2ch.hk.''')
+
+parser.add_argument('MODE', choices=['all', 'images', 'videos'],
+	help='parse mode, e.g. what files to download')
+
+parser.add_argument('URL',
+	help='thread url')
+
+args = parser.parse_args()
+mode = args.MODE
+url = args.URL
+
 
 # Download the content
