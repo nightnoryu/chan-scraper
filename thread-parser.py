c9f11f012d0ad0ac56445b166bb8fc91c2389f68
m3tro1d <qazwf734@gmail.com>
2020-07-20 18:13:06 +0300

    Some formatting

--- thread-parser.py
+++ thread-parser.py
@@ -62,7 +62,8 @@ def save_file(url, directory, name):
 
 # Parse arguments
 parser = argparse.ArgumentParser(
-    description="""Downloads all files, images or videos from the thread on 2ch.hk.""")
+    description="""Downloads all files, images or videos from the
+    thread on 2ch.hk.""")
 
 parser.add_argument("MODE", choices=["all", "images", "videos"],
     help="parse mode, e.g. what files to download")
