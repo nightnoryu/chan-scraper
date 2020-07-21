a1ba31a7e357e9ddec19e0066f08fe62240377ae
m3tro1d <qazwf734@gmail.com>
2020-07-21 09:27:52 +0300

    Tidy up the main file

--- thread-parser.py
+++ thread-parser.py
@@ -2,19 +2,7 @@ import argparse
 import os
 import sys
 
-import requests
-
 import extractors.dvach as dvach
-
-
-BASE_URL = "https://2ch.hk"
-
-
-def get_api_url(url):
-    """Returns an API url (html -> json)"""
-    array = url.split(".")
-    api_url = ".".join(array[:-1])
-    return api_url + ".json"
 
 
 def get_extension(name):
@@ -28,21 +16,6 @@ def is_image(ext):
 
 def is_video(ext):
     return ext == "webm" or ext == "mp4"
-
-
-def count_files(posts, mode="all"):
-    """Counts all files according to the mode"""
-    n = 0
-    for post in posts:
-        for file in post["files"]:
-            ext = get_extension(file["name"])
-            if mode == "images" and is_image(ext):
-                n += 1
-            elif mode == "videos" and is_video(ext):
-                n += 1
-            elif mode == "all":
-                n += 1
-    return n
 
 
 def save_file(url, directory, name):
@@ -75,20 +48,6 @@ mode = args.MODE
 url = args.URL
 directory = os.path.abspath(args.o)
 
-
-# Get the JSON response
-# api_url = get_api_url(url)
-# try:
-#     response = requests.get(api_url)
-# except Exception as ex:
-#     # Handle all requests exceptions
-#     print("Request error: {}".format(ex))
-#     print("Download failed, exiting.")
-#     sys.exit(1)
-# # Parse it down
-# res_json = response.json()
-# posts = res_json["threads"][0]["posts"]
-# amount = count_files(posts, mode)
 
 # Get all the files' URLs
 file_list = dvach.get_files_urls_names(url)
