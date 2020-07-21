d4f49f2c22a56c15f03eabce00a51309f8769d43
m3tro1d <qazwf734@gmail.com>
2020-07-21 09:47:43 +0300

    Add utils file

---
+++ utils.py
@@ -0,0 +1,40 @@
+# This module provides general functions
+
+
+import requests
+
+
+def get_extension(name):
+    return name.split(".")[-1]
+
+
+def is_image(ext):
+    return (ext == "jpg" or ext == "jpeg" or
+        ext == "png" or ext =="gif")
+
+
+def is_video(ext):
+    return ext == "webm" or ext == "mp4"
+
+
+def count_files(files_list, mode="all"):
+    """Returns files amount according to the specified mode"""
+    n = 0
+    for _, file_name in files_list:
+        ext = get_extension(file_name)
+        if ((mode == "images" and is_image(ext)) or
+            (mode == "videos" and is_video(ext)) or
+            (mode == "all")):
+            n += 1
+    return n
+
+
+def save_file(url, directory, name):
+    """Save a file into the specified directory"""
+    # Check if the image already exists
+    full_name = os.path.join(directory, name)
+    if not os.path.isfile(full_name):
+        img_file = open(full_name, "wb")
+        for chunk in requests.get(url):
+            img_file.write(chunk)
+        img_file.close()
