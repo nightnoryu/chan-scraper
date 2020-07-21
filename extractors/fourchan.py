7695185e7ab8e7826833ca704dd98ca3f0f8793a
m3tro1d <qazwf734@gmail.com>
2020-07-21 14:29:44 +0300

    Add a few useful functions and the main core func

--- extractors/fourchan.py
+++ extractors/fourchan.py
@@ -12,3 +12,33 @@ def get_thread_api_url(url):
     """Returns an API URL for a thread"""
     # We're grabbing only board/thread/number
     return THREAD_BASE_URL + "/".join(url.split("/")[3:6]) + ".json"
+
+
+def get_board_name(url):
+    """Returns board's name of the thread URL"""
+    return url.split("/")[3]
+
+
+def get_file_url(post, board_name):
+    """Returns a ready-to-download URL to an image in the post"""
+    return MEDIA_BASE_URL + board_name + "/" + str(post["tim"]) + post["ext"]
+
+
+def get_files_urls_names(url):
+    """Returns a list of tuples (file URL, file name) in thread"""
+    # Get the name of the board
+    board_name = get_board_name(url)
+    # Get the JSON response
+    api_url = get_thread_api_url(url)
+    response = requests.get(api_url)
+    # Parse it down
+    res_json = response.json()
+    posts = res_json["posts"]
+    # Create a list of URLs
+    file_list = []
+    for post in posts:
+        # Not all posts have files, so we'd better check
+        if "tim" in post.keys():
+            name = str(post["tim"]) + post["ext"]
+            file_list.append((get_file_url(post, board_name), name))
+    return file_list
