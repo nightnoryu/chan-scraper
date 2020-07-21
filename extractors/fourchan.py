1bbaf8db60aa8ff5ca4f2e0734249208321da3d9
m3tro1d <qazwf734@gmail.com>
2020-07-21 14:11:23 +0300

    Add thread API URL function

--- extractors/fourchan.py
+++ extractors/fourchan.py
@@ -3,3 +3,9 @@
 
 THREAD_BASE_URL = "https://a.4cdn.org/"
 MEDIA_BASE_URL  = "https://i.4cdn.org/"
+
+
+def get_thread_api_url(url):
+    """Returns an API URL for a thread"""
+    # We're grabbing only board/thread/number
+    return THREAD_BASE_URL + "/".join(url.split("/")[3:6]) + ".json"
