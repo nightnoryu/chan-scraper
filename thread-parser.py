2a8a43e919db179e926c17f9ae5deec35450553d
m3tro1d <qazwf734@gmail.com>
2020-08-23 19:58:42 +0300

    Use parse_thread function

--- thread-parser.py
+++ thread-parser.py
@@ -34,64 +34,66 @@ url = args.URL
 directory = os.path.abspath(args.o)
 
 
+# Parse the thread
+utils.parse_thread(url, mode, directory)
 # Select the extractor
-extractor = None
-if utils.what_board_is_this(url) == "2ch":
-    extractor = dvach
-elif utils.what_board_is_this(url) == "4chan":
-    extractor = fourchan
-else:
-    print("This URL is not supported.")
-    sys.exit(1)
+# extractor = None
+# if utils.what_board_is_this(url) == "2ch":
+#     extractor = dvach
+# elif utils.what_board_is_this(url) == "4chan":
+#     extractor = fourchan
+# else:
+#     print("This URL is not supported.")
+#     sys.exit(1)
 
 
-try:
-    # Get all the files' URLs
-    file_list = extractor.get_files_urls_names(url)
-    # Count the files
-    amount = utils.count_files(file_list, mode)
-except Exception as ex:
-    # Handle requests exceptions
-    print("Request error: {}".format(ex))
-    print("Download failed, exiting.")
-    sys.exit(1)
+# try:
+#     # Get all the files' URLs
+#     file_list = extractor.get_files_urls_names(url)
+#     # Count the files
+#     amount = utils.count_files(file_list, mode)
+# except Exception as ex:
+#     # Handle requests exceptions
+#     print("Request error: {}".format(ex))
+#     print("Download failed, exiting.")
+#     sys.exit(1)
 
 
-# Check if there's any files
-if amount == 0:
-    print("There are no files in this thread: {}".format(url))
-    sys.exit(0)
+# # Check if there's any files
+# if amount == 0:
+#     print("There are no files in this thread: {}".format(url))
+#     sys.exit(0)
 
-# Ask user
-print("{} {} will be saved in the '{}' directory.".format(amount,
-    "file" if amount == 1 else "files",
-    directory))
-choice = input("Proceed (Y/n)? ")
-if not (choice.lower() in ["y", ""]):
-    print("As you wish...")
-    sys.exit(0)
-
-
-# Check the output directory
-if not os.path.isdir(directory):
-    print("\nHmm, '{}' directory doesn't seem to exist...".format(directory))
-    print("Nevermind, I'll just create one.")
-    os.mkdir(directory)
+# # Ask user
+# print("{} {} will be saved in the '{}' directory.".format(amount,
+#     "file" if amount == 1 else "files",
+#     directory))
+# choice = input("Proceed (Y/n)? ")
+# if not (choice.lower() in ["y", ""]):
+#     print("As you wish...")
+#     sys.exit(0)
 
 
-# Actual downloading is happening here
-print("\nDownloading...")
-# Posts loop
-n = 1
-for file_url, file_name in file_list:
-    ext = utils.get_extension(file_url)
-    # Save the files according to the mode
-    if ((mode == "images" and utils.is_image(ext)) or
-        (mode == "videos" and utils.is_video(ext)) or
-        (mode == "all")):
-        # Save the file
-        utils.save_file(file_url, directory, file_name)
-        # Log the action
-        print("{:>3}/{} - {}".format(n, amount, file_name))
-        # Update counter
-        n += 1
+# # Check the output directory
+# if not os.path.isdir(directory):
+#     print("\nHmm, '{}' directory doesn't seem to exist...".format(directory))
+#     print("Nevermind, I'll just create one.")
+#     os.mkdir(directory)
+
+
+# # Actual downloading is happening here
+# print("\nDownloading...")
+# # Posts loop
+# n = 1
+# for file_url, file_name in file_list:
+#     ext = utils.get_extension(file_url)
+#     # Save the files according to the mode
+#     if ((mode == "images" and utils.is_image(ext)) or
+#         (mode == "videos" and utils.is_video(ext)) or
+#         (mode == "all")):
+#         # Save the file
+#         utils.save_file(file_url, directory, file_name)
+#         # Log the action
+#         print("{:>3}/{} - {}".format(n, amount, file_name))
+#         # Update counter
+#         n += 1
