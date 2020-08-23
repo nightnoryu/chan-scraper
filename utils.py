f91169d3c3b2be4b2810b74946f3b9e0bf797bcb
m3tro1d <qazwf734@gmail.com>
2020-08-23 16:24:43 +0300

    Add first version of parse_thread function

--- utils.py
+++ utils.py
@@ -5,6 +5,9 @@ import os
 
 import requests
 
+import extractors.dvach as dvach
+import extractors.fourchan as fourchan
+
 
 def get_extension(name):
     """Returns file's extension without dot"""
@@ -52,3 +55,54 @@ def what_board_is_this(url):
           url.startswith("https://boards.4chan.org")):
         board = "4chan"
     return board
+
+
+def parse_thread(url, mode, directory):
+    """Does the job on one thread"""
+    # Select the extractor
+    extractor = None
+    if what_board_is_this(url) == "2ch":
+        extractor = dvach
+    elif what_board_is_this(url) == "4chan":
+        extractor = fourchan
+    else:
+        print("URL '{}' is not supported.".format(url))
+        return
+
+    try:
+        # Get all the files' URLs
+        file_list = extractor.get_files_urls_names(url)
+        # Count the files
+        amount = count_files(file_list, mode)
+    except Exception as ex:
+        # Handle requests exceptions
+        print("Request error: {}".format(ex))
+        print("Download failed for '{}'.".format(url))
+        return
+
+    # Check if there's any files
+    if amount == 0:
+        print("There are no files in this thread: {}".format(url))
+        return
+
+    # Check the output directory
+    # TODO separate directory with thread's number
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+
+    # Actual downloading is happening here
+    print("\nDownloading...")
+    # Posts loop
+    n = 1
+    for file_url, file_name in file_list:
+        ext = get_extension(file_url)
+        # Save the files according to the mode
+        if ((mode == "images" and is_image(ext)) or
+            (mode == "videos" and is_video(ext)) or
+            (mode == "all")):
+            # Save the file
+            save_file(file_url, directory, file_name)
+            # Log the action
+            print("{:>3}/{} - {}".format(n, amount, file_name))
+            # Update counter
+            n += 1
