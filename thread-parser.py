f5c6fe96297fcd5a2380d29de2ef3a3f79cc04c9
m3tro1d <qazwf734@gmail.com>
2020-09-27 12:39:38 +0300

    Add __name__ check

--- thread-parser.py
+++ thread-parser.py
@@ -14,44 +14,51 @@ import extractors.dvach as dvach
 import extractors.fourchan as fourchan
 
 
-# Parse arguments
-parser = argparse.ArgumentParser(
-    description="""Downloads all files, images or videos from one
-    or several threads on 2ch or 4chan.""")
+def main():
+    """Entry point of the script"""
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description="""Downloads all files, images or videos from one
+        or several threads on 2ch or 4chan.""")
 
-parser.add_argument("MODE", choices=["all", "images", "videos"],
-    help="parse mode, e.g. what files to download")
+    parser.add_argument("MODE", choices=["all", "images", "videos"],
+        help="parse mode, e.g. what files to download")
 
-parser.add_argument("URLS", nargs="*",
-    help="thread urls")
+    parser.add_argument("URLS", nargs="*",
+        help="thread urls")
 
-parser.add_argument("-o", metavar="DIR", default=".",
-    help="output directory (default: current)")
+    parser.add_argument("-o", metavar="DIR", default=".",
+        help="output directory (default: current)")
 
-args = parser.parse_args()
-mode = args.MODE
-urls = args.URLS
-directory = os.path.abspath(args.o)
+    args = parser.parse_args()
+    mode = args.MODE
+    urls = args.URLS
+    directory = os.path.abspath(args.o)
 
 
-# Check input URLS
-if not len(urls):
-    print("Error: no URL provided.")
-    sys.exit(1)
-# Check output directory
-if not os.path.isdir(directory):
-    os.mkdir(directory)
+    # Check input URLS
+    if not len(urls):
+        print("Error: no URL provided.")
+        sys.exit(1)
+    # Check output directory
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
 
-if len(urls) == 1:
-    # Parse single thread
-    utils.parse_thread(urls[0], mode, directory, True)
-else:
-    # Set up counter
-    n = 1
-    total = len(urls)
-    # Parse multiple threads
-    for url in urls:
-        # Log
-        print("\n[{} out of {}]".format(n, total))
-        utils.parse_thread(url, mode, directory)
-        n += 1
+    if len(urls) == 1:
+        # Parse single thread
+        utils.parse_thread(urls[0], mode, directory, True)
+    else:
+        # Set up counter
+        n = 1
+        total = len(urls)
+        # Parse multiple threads
+        for url in urls:
+            # Log
+            print("\n[{} out of {}]".format(n, total))
+            utils.parse_thread(url, mode, directory)
+            n += 1
+
+
+# Entry point
+if __name__ == "__main__":
+    main()
