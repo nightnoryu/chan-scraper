de4811e05171732cb7b0900ded661df604f39ae3
m3tro1d <qazwf734@gmail.com>
2021-08-03 18:56:23 +0300

    Clean up the main file

--- chan-scraper.py
+++ chan-scraper.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
 import sys
 
 from args import parse_arguments
@@ -8,12 +7,10 @@ from scraper import Scraper
 
 
 def main(args):
-    """Entry point of the script"""
     scraper = Scraper(args.urls, args.mode, args.output, args.pause)
     scraper.scrap()
 
 
-# Entry point
 if __name__ == "__main__":
     args = parse_arguments()
     try:
